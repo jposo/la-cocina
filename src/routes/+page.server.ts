043359c07227964cee1ad38280224de9dd2a294c
Juan <jposorniot@gmail.com>
2025-10-27 23:08:34 -0700

    formdata

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,53 +1,53 @@
 import {
-  fetchFoods,
-  likeFood,
-  unlikeFood,
-  getLikedFoods,
+    fetchFoods,
+    likeFood,
+    unlikeFood,
+    getLikedFoods,
 } from "$lib/server/database";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ locals }) => {
-  const foods = await fetchFoods();
-  const user = locals.user;
-  let likes = {};
-  let userId = null;
-  if (user) {
-    userId = user.sub;
-    likes = await getLikedFoods(userId);
-  }
-  console.log(likes);
-  return { foods, likes, userId };
-};
-
-export const actions = {
-  async like({ request, locals }) {
+    const foods = await fetchFoods();
     const user = locals.user;
-
-    if (!user) {
-      return { error: "You must be logged in to like a food." };
+    let likes = {};
+    let userId = null;
+    if (user) {
+        userId = user.sub;
+        likes = await getLikedFoods(userId);
     }
-
-    const userId = user.sub;
-
-    const formData = await request.formData();
-    const foodId = formData.get("foodId");
-    const action = formData.get("action");
-
-    if (!foodId || !userId || typeof foodId !== "string") {
-      return { error: "Invalid request" };
-    }
-
-    try {
-      if (action === "like") {
-        await likeFood(userId, foodId);
-      } else if (action === "unlike") {
-        console.log("unliked");
-        await unlikeFood(userId, foodId);
-      }
-      return { success: true };
-    } catch (error) {
-      console.error(error);
-      return { error: "Failed to like food" };
-    }
-  },
+    console.log(likes);
+    return { foods, likes, userId };
 };
+
+// export const actions = {
+//     async like({ request, locals }) {
+//         const user = locals.user;
+
+//         if (!user) {
+//             return { error: "You must be logged in to like a food." };
+//         }
+
+//         const userId = user.sub;
+
+//         const formData = await request.formData();
+//         const foodId = formData.get("foodId");
+//         const action = formData.get("action");
+
+//         if (!foodId || !userId || typeof foodId !== "string") {
+//             return { error: "Invalid request" };
+//         }
+
+//         try {
+//             if (action === "like") {
+//                 await likeFood(userId, foodId);
+//             } else if (action === "unlike") {
+//                 console.log("unliked");
+//                 await unlikeFood(userId, foodId);
+//             }
+//             return { success: true };
+//         } catch (error) {
+//             console.error(error);
+//             return { error: "Failed to like food" };
+//         }
+//     },
+// };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@
         } else {
             formData.append("action", "unlike");
         }
+        console.log(formData);
         fetch("/api/foods/like", {
             method: "POST",
             body: formData,
